9cba42d45aec1428abf79d26bce6b5957c7cca5a
caiki <marketing@letcode.com.br>
2024-07-16 16:26:59 -0300

    correcao

--- src/app/components/Escolha.tsx
+++ src/app/components/Escolha.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 import { EscolhaItems } from '@/utils/Escolhautils'
 const Escolha = () => {
   return (
-    <div className='lg:w-screen'>
-        <div className=''>
+    <div className='md:w-screen'>
+        <div>
         <div className='bg-bgIntroducao '>
         <h2 className='container pl-10 m-auto drop-shadow text-4xl text-BlueMedio font-Montserrat font-bold'>POR QUE NOS <span className='text-BlueDark'>ESCOLHER?</span></h2>
         </div >
-        <div className=' bg-bgColaborador max-lg:flex lg:justify-center lg:items-center lg:w-full'>
-        <ul className=' bg-bgColaborador xl:flex md:grid md:grid-cols-2 md:justify-center flex justify-center items-center w-screen md:w-screen lg:items-center  self-center max-md:flex-col max-md:justify-center max-md:items-center max-w-7xl mt-[60px] gap-[76px] m-auto text-center max-sm:my-4'>
+        <div className='bg-bgColaborador max-md:flex md:justify-center md:items-center self-center md:w-full'>
+        <ul className=' bg-bgColaborador xl:flex md:grid md:grid-cols-2 md:justify-center flex justify-center items-center w-screen md:w-screen lg:items-center self-center max-md:flex-col max-md:justify-center  max-w-7xl mt-[60px] gap-[76px] m-auto text-center max-sm:my-4'>
         {EscolhaItems.map((item) => (    
             <li key={item.id} className='drop-shadow first:mt-8 tracking-tight '>
                 <h1 className='max-w-[200px] m-auto font-Montserrat text-BlueDark text-[24px] font-bold'>{item.title}</h1>
