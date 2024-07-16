3331de7bb5f2ceb500f6da333c4b19e5c49f4491
caiki <marketing@letcode.com.br>
2024-07-16 14:17:48 -0300

    Correcao-Sistem

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -14,7 +14,7 @@ const Header: React.FC = () => {
           <TiThMenu onClick={() => setOpenMenu(!openMenu)} className='text-4xl mr-4 xl:hidden' />
         </div>
 
-        <ul className={`xl:inline-flex bg-bgFooter p-1 flex flex-col gap-4  md:gap-8 mr-28 text-xl font-roboto-medium md:items-center drop-shadow-md  ${openMenu ? 'top-15 opacity-100 w-screen text-right py-4 z-[1000] pl-4' : ' hidden'} md:opacity-100 opacity-0`}>
+        <ul className={`xl:inline-flex bg-bgFooter p-1 max-sm:flex  max-sm:flex-col gap-4  md:gap-8 mr-28 text-xl font-roboto-medium md:items-center drop-shadow-md  ${openMenu ? 'top-15 opacity-100 w-screen text-right py-4 z-[1000] pl-4' : ' hidden'} md:opacity-100 opacity-0`}>
           {headerItems.map(item => (
             <li key={item.id} className={`${item.text !== 'Área Do Cliente'? 'md:hover:border-b-white md:hover:border-b-2 cursor-pointer': 'md:border-none'}`}>
               {item.text !== 'Área Do Cliente' ? (
